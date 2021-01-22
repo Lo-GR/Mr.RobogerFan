0cfc0e2575c358754d59c6e218b639de7969d6eb
Lo-GR <diamondintheroth@gmail.com>
2021-01-22 14:05:12 -0800

    pass fourth test

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,51 @@
 // function beepBoop(userNumber) {
 //   const numberChosen = parseInt(userNumber);
 //   let numberArray = []
+//   let transformedArray = []
 //   numberArray.push(numberChosen);
-//   for (let index = 0; index < numberChosen; index ++){
+//   for (let i1 = 0; i1< numberChosen; i1 ++){
 //     numberArray.unshift(numberArray[0]-1);
 //   }
-//   return numberArray;
+//   for (const element of numberArray){
+//     let numberString = element.toString();
+//     let varToPush;
+//     for (let i2 = 0; i2 < numberString.length; i2 +=1){
+//       if (numberString.charAt(i2) === "1") {
+//         varToPush = "Beep!"
+//         // transformedArray.push("Beep!");
+//         i2 = numberString.length;
+//       } else {
+//         varToPush = numberString;
+//       };
+//     }
+//     transformedArray.push(varToPush);
+//   };
+//   return transformedArray;
+// };
+
+// function beepBoop(userNumber) {
+//   const numberChosen = parseInt(userNumber);
+//   let numberArray = []
+//   let transformedArray = []
+//   numberArray.push(numberChosen);
+//   for (let i1 = 0; i1< numberChosen; i1 ++){
+//     numberArray.unshift(numberArray[0]-1);
+//   }
+//   for (const element of numberArray){
+//     let numberString = element.toString();
+//     let varToPush = numberString;
+//     for (let i2 = 0; i2 < numberString.length; i2 +=1){
+//       if (numberString.charAt(i2) === "2") {
+//         varToPush = "Boop!"
+//         i2 = numberString.length;
+//       } else if (numberString.charAt(i2) === "1"){
+//         varToPush = "Beep!"
+//         i2 = numberString.length;
+//       };
+//     }
+//     transformedArray.push(varToPush);
+//   };
+//   return transformedArray;
 // };
 
 function beepBoop(userNumber) {
@@ -18,16 +58,12 @@ function beepBoop(userNumber) {
   }
   for (const element of numberArray){
     let numberString = element.toString();
-    let varToPush;
-    for (let i2 = 0; i2 < numberString.length; i2 +=1){
-      if (numberString.charAt(i2) === "1") {
-        varToPush = "Beep!"
-        // transformedArray.push("Beep!");
-        i2 = numberString.length;
-      } else {
-        varToPush = numberString;
-      };
-    }
+    let varToPush = numberString;
+    if (numberString.includes("2")) {
+      varToPush = "Boop!"
+    } else if (numberString.includes("1")){
+      varToPush = "Beep!"
+    };
     transformedArray.push(varToPush);
   };
   return transformedArray;
--- js/specs.js
+++ js/specs.js
@@ -10,8 +10,8 @@ Describe: beepBoop()
 // Expect(beepBoop(0).toEqual([0]);
 // Test: "It should return an array of numbers from 0 to the inputted number"
 // Expect(beepBoop(5).toEqual([0, 1, 2, 3, 4, 5]);
-Test: "It should return an array of numbers from 0 to the inputted number where every number containing a '1' replaces all digits with 'Beep!'"
-Expect(beepBoop(5).toEqual(["Beep!", 2, 3, 4, 5]);
+// Test: "It should return an array of numbers from 0 to the inputted number where every number containing a '1' replaces all digits with 'Beep!'"
+// Expect(beepBoop(5).toEqual(["Beep!", 2, 3, 4, 5]);
 Test: "It should return an array of numbers from 0 to the inputted number where every number containing a '1' replaces all digits of that number with 'Beep!' and every number containing '2' replaces all digits of that number with 'Boop!'"
 Expect(beepBoop(5).toEqual(["Beep!", "Boop!", 3, 4, 5]);
 Test: "It should return an array of numbers from 0 to the inputted number where every number containing a '1' replaces all digits of that number with 'Beep!', every number containing '2' replaces all digits of that number with 'Boop!', and every number containing 3 replaces all digits of that number with 'Won't you be my neighbor?'"
@@ -20,18 +20,26 @@ Expect(beepBoop(5).toEqual(["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5]
 function beepBoop(userNumber) {
   const numberChosen = parseInt(userNumber);
   let numberArray = []
+  let transformedArray = []
   numberArray.push(numberChosen);
-  for (let index = 0; index < numberChosen; index ++){
+  for (let i1 = 0; i1< numberChosen; i1 ++){
     numberArray.unshift(numberArray[0]-1);
   }
-  let transformedArray = numberArray.map(function(element){
+  for (const element of numberArray){
     let numberString = element.toString();
-    for (let index = 0; index < numberString.length; index +=1){
-    if (numberString.charAt(index) = "1") {
-      return "Beep!";
-    } else {
-      return element;
+    let varToPush;
+    for (let i2 = 0; i2 < numberString.length; i2 +=1){
+      if (numberString.charAt(i2) === "2") {
+        varToPush = "Boop!"
+        i2 = numberString.length;
+      } else if (numberString.charAt(i2) === "1"){
+        varToPush = "Beep!"
+        i2 = numberString.length;
+      } else {
+        varToPush = numberString;
+      };
     }
-  }});
+    transformedArray.push(varToPush);
+  };
   return transformedArray;
 };
