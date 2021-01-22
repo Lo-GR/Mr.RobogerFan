ee3d454fd52dcf0270bffb414aa657074829e421
Lo-GR <diamondintheroth@gmail.com>
2021-01-22 10:28:40 -0800

    pass second beepBoop test

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,10 @@
 function beepBoop(userNumber) {
+  const numberChosen = parseInt(userNumber);
   let numberArray = []
-  numberArray.push(parseInt(userNumber));
+  numberArray.push(numberChosen);
+  for (let index = 0; index < numberChosen; index ++){
+    numberArray.unshift(numberArray[0]-1);
+  }
   return numberArray;
 };
-
-console.log(beepBoop("2"))
+console.log(beepBoop("5"))
--- js/specs.js
+++ js/specs.js
@@ -8,8 +8,8 @@ Numbers that contain a 3: all digits are replaced (all digits) with "Won't you b
 Describe: beepBoop()
 // Test: "It should return an array with a 0 if the number 0 is inputted"
 // Expect(beepBoop(0).toEqual([0]);
-Test: "It should return an array of numbers from 0 to the inputted number"
-Expect(beepBoop(5).toEqual([0, 1, 2, 3, 4, 5]);
+// Test: "It should return an array of numbers from 0 to the inputted number"
+// Expect(beepBoop(5).toEqual([0, 1, 2, 3, 4, 5]);
 Test: "It should return an array of numbers from 0 to the inputted number where every number containing a '1' replaces all digits with 'Beep!'"
 Expect(beepBoop(5).toEqual(["Beep!", 2, 3, 4, 5]);
 Test: "It should return an array of numbers from 0 to the inputted number where every number containing a '1' replaces all digits of that number with 'Beep!' and every number containing '2' replaces all digits of that number with 'Boop!'"
@@ -19,7 +19,12 @@ Expect(beepBoop(5).toEqual(["Beep!", "Boop!", 3, 4, 5]);
 Test: "It should return an array of numbers from 0 to the inputted number where every number containing a '1' replaces all digits of that number with 'Beep!', every number containing '2' replaces all digits of that number with 'Boop!', and every number containing 3 replaces all digits of that number with 'Won't you be my neighbor?'"
 Expect(beepBoop(5).toEqual(["Beep!", "Boop!", "Won't you be my neighbor?", 4, 5]);
 
-beepBoop(userNumber){
+function beepBoop(userNumber) {
+  const numberChosen = parseInt(userNumber);
   let numberArray = []
-  numberArray.push(parseInt(userNumber));
-}
+  numberArray.push(numberChosen);
+  for (let index = 0; index < numberChosen; index ++){
+    numberArray.unshift(numberArray[0]-1);
+  }
+  return numberArray;
+};
